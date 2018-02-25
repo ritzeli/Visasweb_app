d615b3a24cea5240aa9b1844e7b17f769144756e
GUILLERMO PULIDO SARMIENTO <guillermo@GUILLERMOs-Air.gateway>
2018-02-26 09:11:24 +1000

    fix the chabot and new configuration of tsconfig.app.json

--- src/app/services/chatbot/chatbot.service.ts
+++ src/app/services/chatbot/chatbot.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@angular/core';
+import {ApiAiClient} from "api-ai-javascript";
+import {environment} from '../../../environments/environment';
+
 
 @Injectable()
 export class ChatbotService {
+  readonly token = environment.dialogflow.angularBot;
+  readonly client = new ApiAiClient({ accessToken: this.token }).textRequest('Hola!')
+    .then((response) => {console.log(response)})
+    .catch((error) => {/* do something here too */});
   tex: any = '' ;
   constructor() {
     this.tex = 'Hola desde el servicio chat-bot funcionando desde el index';
