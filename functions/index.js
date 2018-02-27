6af9987cf5beae97d2e777387bdbb08bd9d2753c
memo <ingepulido@hotmail.com>
2018-02-27 14:16:06 +1000

    new express functions is now working

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,41 @@
 const functions = require('firebase-functions');
-const http = require('requestify');
+//const http = require('requestify');
 const cors = require('cors')({ origin: true });
 const apiai = require('apiai');
-
-exports.chatbot = functions.https.onRequest((request, response) => {
+const express = require('express');
+const admin = require('firebase-admin');
+const app = express();
+// initialize_app
+admin.initializeApp(functions.config().firebase);
+// Automatically allow cross-origin requests
+app.get('/chatbot', (request, response) => {
   cors(request, response, () => {
-    const app = apiai(functions.config().apiai.key);
+   const app = apiai(functions.config().apiai.key);
     const message = request.query.message;
-    const User_id = request.query.User_id;
+   const User_id = request.query.User_id;
 
     let request2= app.textRequest(message, {
-      sessionId: User_id
+     sessionId: User_id
     });
 
-    request2.on('response', (r) => {
-      console.log(r);
+   request2.on('response', (r) => {
+     console.log(r);
       return response.status(200).json({
-        message_test:r
+       message_test:r
       })
     });
     request2.on('error', (error) => {
       console.log(error);
     });
-    request2.end();
-
+   request2.end();
   });
 });
+app.get('/chat', function (req, res) {
+  res.send('Hello from chat2')
+});
+
+exports.visasweb = functions.https.onRequest(app);
+
+//exports.chatbot = functions.https.onRequest((request, response) => {
+
+//});
--- src/app/services/chatbot/chatbot.service.ts
+++ src/app/services/chatbot/chatbot.service.ts
@@ -4,14 +4,14 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 @Injectable()
 export class ChatbotService {
   tex: any = '' ;
-  readonly ROOT_URL = 'https://us-central1-visasweb-57098.cloudfunctions.net/chatbot'
+  readonly ROOT_URL = 'https://us-central1-visasweb-57098.cloudfunctions.net/visasweb/chatbot'
   constructor(private http: HttpClient) {
     this.tex = 'Hola desde el servicio chat-bot funcionando desde el index';
   }
   test_conexion() {
     let params = new HttpParams();
     params = params.set('message', 'hola');
-    params = params.set('User_id', 'xdcsdfs123');
+    params = params.set('User_id', '123456');
     return this.http.get(this.ROOT_URL, {params});
   }
 }
